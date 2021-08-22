670f7bd3675c2b2092f0c3ae0e98dacbd9fc0c65
Nico <nicholasgutierrez95@gmail.com>
2021-08-22 12:41:53 -0700

    init

--- sketch.js
+++ sketch.js
@@ -1,18 +1,8 @@
 function setup() {
   // put setup code here
   createCanvas(720, 720);
-  noFill();
-  //fill(0);
-  //stroke(255);
-  slider = createSlider(10,100,50);
-  console.log("Hello, there...");
 }
 
 function draw() {
   background(0);
-  stroke(255);
-  strokeWeight(10);
-  // put drawing code here
-  console.log("drawing...");
-  ellipse(width/2,height/2,slider.value(),slider.value());
 }
